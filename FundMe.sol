1d11c94502c5afdccca922d63a34ea181f021390
Arthur <ac.kibon@gmail.com>
2024-03-06 15:59:41 -0300

    Update FundMe.sol + withdraw function

--- FundMe.sol
+++ FundMe.sol
@@ -1,29 +1,70 @@
-// Get funds from users
-// Withdraw funds
-// Set a minimum funding value in USD
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
 
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+// Contract Address 0x6d31505C35EAaE8e84464375AaA5e9C9c6e0054F
+
 import {PriceConverter} from "./PriceConverter.sol";
+
+error NotOwner();
 
 contract FundMe {
 
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
-    function fund() public payable{
-        require(msg.value.getConversionRate() > minimumUsd, "didn't send enough ETH");
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
     }
 
-    // function withdraw() public {} TODO
+    function fund() public payable{
+        require(msg.value.getConversionRate() > MINIMUM_USD, "didn't send enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        
+        funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+    }
 
     
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "Sender is not Owner!");
+        if(msg.sender != i_owner) {revert NotOwner();}
+        _;
+    }
+
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    } 
+
 }
